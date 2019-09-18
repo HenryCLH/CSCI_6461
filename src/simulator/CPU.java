5d06ab4dc8e159c242cd35ef1c13595e9f55a727
HenryCLH <henrychenglh@gmail.com>
2019-09-18 18:37:43 -0400

    fix faults and add comments

--- src/simulator/CPU.java
+++ src/simulator/CPU.java
@@ -2,8 +2,8 @@ package simulator;
 
 public class CPU
 {
-	private int[] Reg;	// General Purpose Register 16 bits
-	private int[] XReg;	// Index Register 16 bits
+	private int[] Reg;	// General Purpose Register (GPR) 16 bits
+	private int[] XReg;	// Index Register (XR) 16 bits
 
 	private int IR;	// Instruction Register 16 bits
 	private int PC;	// Program Counter 12 bits
@@ -14,116 +14,100 @@ public class CPU
 	private int MBR;	// Memory Buffer Register 16 bits
 	private int MFR;	// Machine Fault Register 4 bits
 
-	// Instruction
+	// Instruction structure
+	// Now is just load/store structure
 	private int Opcode;		// 6 bits
 	private int R;			// 2 bits
 	private int IX;			// 2 bits
 	private int I;			// 1 bits
 	private int Address;	// 5 bits
 
-	// Memory
+	// Reference of Memory object
 	private Memory mem;
 
 	CPU(Memory m)
 	{
-		System.out.println("--------CPU  Start--------");	// print for debug
-		mem = m;	// connect to the memory
-		Reg = new int[] { 0, 0, 0, 0 };
-		XReg = new int[] { 0, 0, 0 };
-		run();
+		System.out.println("--------CPU  Start--------"); // print for debug
+		mem = m; // connect to the memory
+		Reg = new int[] { 0, 0, 0, 0 }; // initiate GPRs with 0
+		XReg = new int[] { 0, 0, 0 }; // initiate XRs with 0
 	}
 
-	// set PC
+	// set the value of PC
 	public void set_PC(int pc)
 	{ PC = pc; }
 
-	// set register
+	// set the value of GPRs
 	public void set_register(int reg, int num)
 	{ Reg[num] = reg; }
 
-	// run
-	public void run()
+	// run test program, PC has the address of the first instruction of the test program
+	public void run(int pc)
 	{
-		// TODO
+		PC = pc; // set PC
+		IR = mem.load(PC); // load instruction
+		while (IR != 0) // let 0 value means the end of the test program
+		{
+			decoder(); // decode and do the instruction, now is just load/store
+			IR = mem.load(PC); // get next instruction
+		}
 	}
 
 	// single step run
 	public void step()
 	{
-		IR = mem.load(PC);
-		decoder();
+		IR = mem.load(PC); // load instruction
+		decoder(); // decode and do the instruction, now is just load/store
 	}
 
+	// main part of the CPU, decode and do instructions
 	// decode the instruction
 	public void decoder()
 	{
-		System.out.println("-------Decoder");
+		// decode the instruction
+		Opcode = (IR & 0b1111110000000000) >> 10; // get Opcode
+		// load/store instruction structure
+		R = (IR & 0b1100000000) >> 8; // index of GPR
+		IX = (IR & 0b11000000) >> 6; // index of XR
+		I = (IR & 0b100000) >> 5; // sign of indirect
+		Address = IR & 0b11111; // address
 
-		int tmpcode = (IR & 0b1100000000000000) >> 14;
-		switch (tmpcode)
+		// calculate effective address (EA) and store in MAR
+		if (I == 0) // not indirect addressing
 		{
-			case 0: // 6 bits Opcode
-			{
-				Opcode = (IR & 0b1111110000000000) >> 10;
-				R = (IR & 0b1100000000) >> 8;
-				IX = (IR & 0b11000000) >> 6;
-				I = (IR & 0b100000) >> 5;
-				Address = IR & 0b11111;
-				// calculate effective address
-				if (I == 0)
-				{
-					if (IX == 0)
-						MAR = Address;
-					else
-						MAR = Address + mem.load(XReg[IX]);
-				}
-				else
-				{
-					if (IX == 0)
-						MAR = mem.load(Address);
-					else
-						MAR = mem.load(Address + mem.load(XReg[IX]));
-				}
-				// decode Opcode
-				switch (Opcode)
-				{
-					case 1: // LDR R, IX, Address
-						Reg[R] = mem.load(MAR);
-						break;
-					case 2: // STR R, IX, Address
-						mem.store(MAR, Reg[R]);
-						break;
-					case 3: // LDA R, IX, Address
-						Reg[R] = MAR;
-						break;
-				}
-				break;
-			}
-			case 1: // 8 bits Opcode
-			{
-				Opcode = (IR & 0b1111111100000000) >> 8;
-				IX = (IR & 0b11000000) >> 6;
-				I = (IR & 0b100000) >> 5;
-				Address = IR & 0b11111;
-				// calculate effective address
-				if (I == 0)
-					MAR = Address;
-				else
-					MAR = mem.load(Address);
-				// decode Opcode
-				switch (Opcode)
-				{
-					case 0x41: // LDX IX, Address
-						XReg[IX] = mem.load(MAR);
-						break;
-					case 0x42: // STX IX, Address
-						mem.store(MAR, XReg[IX]);
-						break;
-				}
-				break;
-			}
+			if (IX == 0) // without index register
+				MAR = Address;
+			else // use index register
+				MAR = Address + mem.load(XReg[IX]);
 		}
-		PC++; // set PC to next instruction
+		else // indirect addressing
+		{
+			if (IX == 0) // without index register
+				MAR = mem.load(Address);
+			else // use index register
+				MAR = mem.load(Address + mem.load(XReg[IX]));
+		}
+
+		switch (Opcode)
+		{
+			case 1: // LDR R, IX, Address
+				Reg[R] = mem.load(MAR); // load data from memory into GPR
+				break;
+			case 2: // STR R, IX, Address
+				mem.store(MAR, Reg[R]); // store GPR data into memory
+				break;
+			case 3: // LDA R, IX, Address
+				Reg[R] = MAR; // load just the address into GPR
+				break;
+			case 041: // LDX IX, Address
+				XReg[IX] = mem.load(MAR); // load data from memory into XR
+				break;
+			case 042: // STX IX, Address
+				mem.store(MAR, XReg[IX]); // store XR data into memory
+				break;
+		}
+
+		PC++; // set PC point to next instruction
 
 		// print for debug
 		System.out.println("Opcode:\t" + toBinaryString(Opcode, 6));
@@ -134,15 +118,7 @@ public class CPU
 		System.out.println("PC:\t" + toBinaryString(PC, 12));
 	}
 
-	// load
-	public int load(int address)
-	{ return mem.load(address); }
-
-	// store
-	public void store(int address, int value)
-	{ mem.store(address, value); }
-
-	// function to print registers in binary string
+	// just for debug, will not be in the final program
 	public String toBinaryString(int num, int bits)
 	{
 		String s = Integer.toBinaryString(num);
--- src/simulator/simulator.java
+++ src/simulator/simulator.java
@@ -7,9 +7,12 @@ public class simulator
 
 	public static void main(String[] args)
 	{
-		memory.store(0, 0b11100011111);
-		cpu.set_PC(0);
-		cpu.step();
+		memory.store(0b11111, 123);
+		memory.store(0, 0b0000011100011111);
+		memory.store(1, 0b0000101100011110);
+		cpu.run(0);
+		System.out.println(memory.load(0b11111));
+		System.out.println(memory.load(0b11110));
 	}
 
 }
